9110408fb4792df4eb55018c264e141ddf56d08f
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-05-29 14:34:55 -0300

    feat: add beecrowd 1019

--- src/beecrowd_1019.rs
+++ src/beecrowd_1019.rs
@@ -1,1 +1,15 @@
-fn main() {}
+// https://www.beecrowd.com.br/judge/pt/problems/view/1018
+
+use std::io;
+
+fn main() {
+    let t = input_line();
+
+    println!("{}:{}:{}", t / 3600, (t % 3600) / 60, ((t % 3600) % 60))
+}
+
+fn input_line() -> i32 {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    return line.trim().parse().unwrap_or_default();
+}
