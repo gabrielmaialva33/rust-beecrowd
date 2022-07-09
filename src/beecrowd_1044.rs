818841bb0ed23e9a46b064792922a6abaef25443
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-07-09 20:40:39 -0300

    feat: add beecrowd 1044

---
+++ src/beecrowd_1044.rs
@@ -0,0 +1,26 @@
+// https://www.beecrowd.com.br/judge/pt/problems/view/1043
+
+use std::convert::TryFrom;
+
+fn main() {
+    let (a, b) = input_line();
+    if a % b == 0 {
+        println!("Sao Multiplos")
+    } else {
+        println!("Nao sao Multiplos")
+    }
+}
+
+fn input_line() -> (i64, i64) {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    let mut v = input
+        .trim()
+        .split_whitespace()
+        .map(|s| s.trim().parse::<i64>().unwrap_or_default())
+        .collect::<Vec<i64>>();
+    v.sort_by(|a, b| b.cmp(a));
+    let [x, y]: [i64; 2] = <[i64; 2]>::try_from(v.as_slice()).unwrap();
+    return (x, y);
+}
