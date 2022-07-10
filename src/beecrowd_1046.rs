ddbb7bf6b57ca0d821350e36c494c412588c3b09
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-07-10 00:31:52 -0300

    feat: add beecrowd 1046

---
+++ src/beecrowd_1046.rs
@@ -0,0 +1,30 @@
+// https://www.beecrowd.com.br/judge/pt/problems/view/1046
+
+use std::convert::TryFrom;
+
+fn main() {
+    let (init_hour, end_hour) = input_line();
+    if init_hour >= end_hour {
+        println!(
+            "O JOGO DUROU {} HORA(S)",
+            ((init_hour - 24) - end_hour) * -1
+        );
+    } else {
+        println!("O JOGO DUROU {} HORA(S)", ((init_hour) - end_hour) * -1);
+    }
+}
+
+fn input_line() -> (i64, i64) {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    let [x, y]: [i64; 2] = <[i64; 2]>::try_from(
+        input
+            .split_whitespace()
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>(),
+    )
+    .unwrap();
+
+    return (x, y);
+}
