c937ec3f610cfb8a4375cecf670ec4ce36d2a9be
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-07-10 15:26:27 -0300

    feat: add beecrowd 1066

---
+++ src/beecrowd_1066.rs
@@ -0,0 +1,27 @@
+// https://www.beecrowd.com.br/judge/pt/problems/view/1065
+
+fn main() {
+    let (mut even, mut odd, mut positive, mut negative) = (0, 0, 0, 0);
+    for _ in 0..5 {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).ok();
+        let value = input.trim().parse::<i64>().unwrap();
+        if value % 2 == 0 {
+            even += 1;
+        }
+        if value % 2 != 0 {
+            odd += 1;
+        }
+        if value > 0 {
+            positive += 1;
+        }
+        if value < 0 {
+            negative += 1;
+        }
+    }
+
+    println!("{} valor(es) par(es)", even);
+    println!("{} valor(es) impar(es)", odd);
+    println!("{} valor(es) positivo(s)", positive);
+    println!("{} valor(es) negativo(s)", negative);
+}
