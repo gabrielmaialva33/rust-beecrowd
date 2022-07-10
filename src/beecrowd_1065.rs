5bdf34e86143f65ecb0fa0a89f8cfa11ffd3b43a
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-07-10 15:21:13 -0300

    feat: add beecrowd 1065

--- src/beecrowd_1065.rs
+++ src/beecrowd_1065.rs
@@ -1,1 +1,15 @@
-fn main() {}
+// https://www.beecrowd.com.br/judge/pt/problems/view/1065
+
+fn main() {
+    let mut count = 0;
+    for _ in 0..5 {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).ok();
+        let value = input.trim().parse::<i64>().unwrap();
+        if value % 2 == 0 {
+            count += 1;
+        }
+    }
+
+    println!("{} valores pares", count);
+}
