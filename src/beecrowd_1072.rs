26ab1b7d5663397dad92aa2a9ea493612f093bae
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-07-24 00:03:42 -0300

    feat: add beecrowd 1072

---
+++ src/beecrowd_1072.rs
@@ -0,0 +1,14 @@
+// https://www.beecrowd.com.br/judge/pt/problems/view/1072
+
+fn main() {
+    let mut n = String::new();
+    std::io::stdin().read_line(&mut n).ok();
+    let n = n.trim().parse::<i64>().unwrap();
+    let mut vec: Vec<i64> = Vec::new();
+    for _ in 0..n {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).ok();
+        let value = input.trim().parse::<i64>().unwrap();
+        vec.push(value);
+    }
+}
